288e902504d7f42b08dc2d1ca6af526db4e42759
Duc <ngohoaiduc08@gmail.com>
2025-05-20 20:28:46 +0700

    Done: Get topics

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Express } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
+import Topic from "./models/topic.model";
 
 dotenv.config();
 database.connect();
@@ -11,8 +12,17 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", (req: Request, res: Response) => {
-  res.render("client/pages/topics/index.pug");
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false,
+  });
+
+  console.log(topics);
+
+  res.render("client/pages/topics/index.pug", {
+    title: "Trang chủ đề bài hát",
+    topics: topics,
+  });
 });
 
 app.listen(port, () => {
