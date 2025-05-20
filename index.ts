f6c0e94e624bb03bf2d4804fe775389f327efd9a
Duc <ngohoaiduc08@gmail.com>
2025-05-20 17:16:13 +0700

    Done: Create bassic route

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Ok");
+import express, { Request, Response, Express } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.send("Đây là trang chủ đề");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
