c7ae77a3958f33c00b7e40786a3d9ba07838b10e
Duc <ngohoaiduc08@gmail.com>
2025-05-20 21:03:26 +0700

    Done: Create Route

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Express } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
-import Topic from "./models/topic.model";
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
 database.connect();
@@ -12,18 +12,7 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/topics", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false,
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index.pug", {
-    title: "Trang chủ đề bài hát",
-    topics: topics,
-  });
-});
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
---
+++ routes/client/topic.route.ts
@@ -0,0 +1,19 @@
+import { Request, Response, Router } from "express";
+const router: Router = Router();
+
+import Topic from "../../models/topic.model";
+
+router.get("/", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false,
+  });
+
+  console.log(topics);
+
+  res.render("client/pages/topics/index.pug", {
+    title: "Trang chủ đề bài hát",
+    topics: topics,
+  });
+});
+
+export const topicRouter: Router = router;
