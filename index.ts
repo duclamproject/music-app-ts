6f91a956c3feb11aa2a951d6e05a44595cb96bb4
Duc <ngohoaiduc08@gmail.com>
2025-05-20 17:38:58 +0700

    Done: Views

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import express, { Request, Response, Express } from "express";
 const app: Express = express();
 const port: number = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/topics", (req: Request, res: Response) => {
-  res.send("Đây là trang chủ đề");
+  res.render("client/pages/topics/index.pug");
 });
 
 app.listen(port, () => {
