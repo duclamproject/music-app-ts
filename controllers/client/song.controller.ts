f05eea933ced2720d9f1b0bf781d101f91d69b3b
Duc <ngohoaiduc08@gmail.com>
2025-05-21 10:12:47 +0700

    Done: Detail song

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -33,3 +33,33 @@ export const list = async (req: Request, res: Response) => {
     songs: songs,
   });
 };
+
+//[GET]: /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+  const slugSong = req.params.slugSong;
+
+  const song = await Song.findOne({
+    slug: slugSong,
+    status: "active",
+    deleted: false,
+  });
+
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    status: "active",
+    deleted: false,
+  }).select("fullName");
+
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    status: "active",
+    deleted: false,
+  }).select("title");
+
+  res.render("client/pages/songs/detail", {
+    pageTitle: song.title,
+    song: song,
+    singer: singer,
+    topic: topic,
+  });
+};
--- routes/client/song.route.ts
+++ routes/client/song.route.ts
@@ -3,5 +3,6 @@ const router: Router = Router();
 
 import * as controller from "../../controllers/client/song.controller";
 router.get("/:slugTopic", controller.list);
+router.get("/detail/:slugSong", controller.detail);
 
 export const songRoutes = router;
