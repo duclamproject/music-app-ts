31a6bbc7e16fb0a2773cff0791d056d8cc4974f8
Duc <ngohoaiduc08@gmail.com>
2025-05-31 00:11:03 +0700

    Done: Layout add song (ADMIN)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,13 @@
-console.log("Ok");
+// Upload Preview
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End Upload Preview
--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -3,5 +3,6 @@ const router: Router = Router();
 import * as controller from "../../controllers/admin/song.controller";
 
 router.get("/", controller.index);
+router.get("/create", controller.create);
 
 export const songRoutes = router;
